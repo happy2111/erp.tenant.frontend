393e2b2e2bf3c5b7054bb3304cc4769286f4a942
Ping <muhammadyusufa2111@gmail.com>
2025-12-14 12:05:23 +0500

    added brand, category and product category schemas and services

---
+++ src/services/brand.service.ts
@@ -0,0 +1,35 @@
+import {
+  CreateBrandDto,
+  UpdateBrandDto,
+  BrandFilterDto,
+  Brand,
+  PaginatedBrands
+} from "@/schemas/brand.schema";
+import api from "@/lib/axiosInstance";
+
+export class BrandService {
+  async create(dto: CreateBrandDto): Promise<Brand> {
+    const res = await api.post("/brands/create", dto);
+    return res.data.data;
+  }
+
+  async findAll(filter: BrandFilterDto): Promise<PaginatedBrands> {
+    const res = await api.post("/brands/filter", filter);
+    return res.data.data;
+  }
+
+  async findOne(id: string): Promise<Brand> {
+    const res = await api.get(`/brands/${id}`);
+    return res.data.data;
+  }
+
+  async update(id: string, dto: UpdateBrandDto): Promise<Brand> {
+    const res = await api.patch(`/brands/update/${id}`, dto);
+    return res.data.data;
+  }
+
+  async remove(id: string): Promise<void> {
+    const res = await api.delete(`/brands/remove/${id}`);
+    return res.data.data;
+  }
+}
